09ef21d60b7d0da7b6f7d4ce5ca25909a5db5449
j0053f <jooo3f@gmail.com>
2023-09-09 19:23:44 +0330

    style change

--- src/components/Linkpreview.tsx
+++ src/components/Linkpreview.tsx
@@ -9,10 +9,10 @@ export default function Linkpreview({
   const { image, title } = data;
   return (
     <div className="w-[30rem] rounded ">
-      <img src={`https://test-url.ir/api/${image}`} className="rounded-md" />
+      <img src={`https://test-url.ir/api/${image}`} className="rounded-t-lg" />
 
       <div
-        className={`text-lg font-bold  ${
+        className={`px-2 pt-3 text-lg font-bold  ${
           title.charCodeAt(0) > 1000 ? "text-right" : "text-left"
         }`}
       >
